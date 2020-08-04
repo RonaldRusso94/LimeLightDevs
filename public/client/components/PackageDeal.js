907caeb0bd41e75ade913d703fefa659f4f76343
Ronald Russo <RonaldRusso94@yahoo.com>
2020-08-04 19:34:59 -0400

    PackageDeal.js fixed text wrapping *Still needs resonsive margins/padding

--- public/client/components/PackageDeal.js
+++ public/client/components/PackageDeal.js
@@ -2,25 +2,25 @@ import Button from "./common/Button";
 
 const PackageDeal = ({ title, originalPrice, price, description, details }) => {
   return (
-    <div className="bg-white text-center rounded sm:px-2 px-4 ">
-      <h2 className="text-2xl mb-2 mt-6">{title}</h2>
+    <div className="bg-white text-center rounded sm:px-2 px-4 py-2 flex flex-col justify-between">
+      <h2 className="text-2xl ">{title}</h2>
       <p className="line-through">{originalPrice}</p>
-      <p className=" text-3xl font-bold mb-4">{price}</p>
-      <p className="mb-6">{description}</p>
+      <p className=" text-3xl font-bold ">{price}</p>
+      <p className="">{description}</p>
 
       <Button className={"m-4"}>Order Now</Button>
 
       {details &&
         details.map((detail) => {
           return (
-            <p className="mt-2" key={detail}>
+            <p className="" key={detail}>
               {detail}
             </p>
           );
         })}
-      <div className="mt-4">
+      <div className="">
         <hr />
-        <div className="flex justify-around py-4">
+        <div className="flex justify-around">
           <p>Live Chat</p>
           <p>(555) 555-5555</p>
         </div>
