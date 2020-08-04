7adb1e7a278dcc8555a76653837d3f2e63c2c7c2
Ronald Russo <RonaldRusso94@yahoo.com>
2020-08-04 12:58:26 -0400

    Update index.js

--- public/client/pages/index.js
+++ public/client/pages/index.js
@@ -1,23 +1,26 @@
 import Carousel from "../components/Carousel";
+import Layout from "../components/layout/Layout";
 
 class index extends React.Component {
   render() {
     return (
-      <div className="flex">
-        <div className=" text-center w-1/2 bg-red-100 px-4">
-          <h1 className=" text-4xl">Short Striking Bold Text</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Perferendis, itaque?
-          </p>
-        </div>
+      <Layout>
+        <div className="flex pt-4">
+          <div className=" text-center w-1/2 bg-red-100 px-4">
+            <h1 className=" text-4xl">Short Striking Bold Text</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Perferendis, itaque?
+            </p>
+          </div>
 
-        <div className="w-1/2">
-          <div className="bg-blue-500 ">
-            <Carousel />
+          <div className="w-1/2">
+            <div className="bg-blue-500 ">
+              <Carousel />
+            </div>
           </div>
         </div>
-      </div>
+      </Layout>
     );
   }
 }
