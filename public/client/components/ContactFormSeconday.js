d427b482c820271779927f7dcf743a65812eb8de
Ronald Russo <RonaldRusso94@yahoo.com>
2020-08-06 11:58:44 -0400

    ContactFormSecondary *Still needs work

---
+++ public/client/components/ContactFormSeconday.js
@@ -0,0 +1,20 @@
+import ContactForm from "./ContactForm";
+const ContactFormSeconday = () => {
+  return (
+    <div className="bg-purple-600 py-4 flex">
+      <div className="w-1/2">a</div>
+
+      <div className=" w-1/2">
+        <h2 className=" text-2xl">Lets Do Great Things Together</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure
+          reprehenderit quaerat accusantium distinctio. In voluptatem commodi
+          aliquam harum voluptatum possimus?
+        </p>
+        <ContactForm />
+      </div>
+    </div>
+  );
+};
+
+export default ContactFormSeconday;
--- public/client/pages/index.js
+++ public/client/pages/index.js
@@ -3,14 +3,17 @@ import Hero from "../components/Hero";
 import Button from "../components/common/Button";
 import PackageDeal from "../components/PackageDeal";
 import Packages from "../components/Packages";
+import ContactFormSecondary from "../components/ContactFormSeconday";
 
 class index extends React.Component {
   render() {
     return (
       <Layout>
-        <div className="md:mx-8 mx-1">
+        <div className="md:mx-8 mx-1 bg-green-400 p-4">
+          Index.js
           {/* <Hero /> */}
           <Packages />
+          <ContactFormSecondary />
         </div>
       </Layout>
     );
