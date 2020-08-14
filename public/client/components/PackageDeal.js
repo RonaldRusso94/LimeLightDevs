49b1304f024ef8edbf74e5670de9775323b84c16
kacper.sledz <casper.sledx@gmail.com>
2020-08-14 13:23:10 +0200

    Generic Package Component and Package Groups

--- public/client/components/PackageDeal.js
+++ public/client/components/PackageDeal.js
@@ -4,8 +4,8 @@ const PackageDeal = ({ title, originalPrice, price, description, details }) => {
   return (
     <div className="bg-white text-center rounded px-4 sm:px-2 xl:px-8 py-2 flex flex-col justify-between shadow-xl max-w-xs">
       <h2 className="text-2xl py-3">{title}</h2>
-      <p className="line-through">{originalPrice}</p>
-      <p className=" text-3xl font-bold py-1">{price}</p>
+      <p className="line-through">{`$${originalPrice}`}</p>
+      <p className=" text-3xl font-bold py-1">{`$${price}`}</p>
       <p className="py-1">{description}</p>
 
       <Button className={"m-3"}>Order Now</Button>
--- public/client/components/Packages.js
+++ public/client/components/Packages.js
@@ -1,6 +1,81 @@
 import PackageDeal from "./PackageDeal";
+import { useState, useEffect } from "react";
+
+// TODO get this from server
+const packageGroupsData = [
+  {
+    name: "Web Design",
+    packages: [
+      {
+        title: "Basic Web Package",
+        originalPrice: "299",
+        price: "199",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, voluptates?",
+        details: ["2 Stock Images", "3 Page Website"],
+      },
+      {
+        title: "Startup Web Package",
+        originalPrice: "596",
+        price: "399",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, voluptates?",
+        details: ["5 Stock Photos", "5 Page Website"],
+      },
+      {
+        title: "Professional Web Package",
+        originalPrice: "1308",
+        price: "699",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, voluptates?",
+        details: ["10 Stock Photos", "10 Page Website"],
+      },
+    ],
+  },
+  {
+    name: "eCommerce Packages",
+    packages: [
+      {
+        title: "Basic eCommerce Package",
+        originalPrice: "299",
+        price: "199",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, voluptates?",
+        details: ["2 Stock Images", "3 Page Website"],
+      },
+      {
+        title: "Startup eCommerce Package",
+        originalPrice: "596",
+        price: "399",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, voluptates?",
+        details: ["5 Stock Photos", "5 Page Website"],
+      },
+      {
+        title: "Professional eCommerce Package",
+        originalPrice: "1308",
+        price: "699",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, voluptates?",
+        details: ["10 Stock Photos", "10 Page Website"],
+      },
+    ],
+  },
+];
 
 const Packages = () => {
+  const [packageGroups] = useState(packageGroupsData);
+  const [activeGroup, setActiveGroup] = useState("");
+  const [visiblePackageGroup, setVisiblePackageGroup] = useState();
+
+  useEffect(() => {
+    setActiveGroup(packageGroups?.[0]?.name);
+  }, [packageGroups]);
+
+  useEffect(() => {
+    setVisiblePackageGroup(packageGroups?.find((p) => p.name == activeGroup));
+  }, [activeGroup, packageGroups]);
+
   return (
     <div className="bg-gray-200 mt-16 p-4">
       <div>
@@ -13,44 +88,39 @@ const Packages = () => {
         </p>
       </div>
 
+      <PackageGroupSelector
+        {...{
+          activeGroup,
+          setActiveGroup,
+          groups: packageGroups?.map((group) => group.name),
+        }}
+      />
+
       <div className="grid grid-cols-1 sm:grid-cols-3 lg:gap-8 gap-4 xl:gap-0 mx-8 sm:mx-0 lg:mx-6 xl:mx-64 mt-8">
-        <PackageDeal
-          title={"Basic Web Package"}
-          originalPrice={"$299"}
-          price={"$199"}
-          description={
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, voluptates?"
-          }
-          details={[
-            "2 Stock Images",
-            "3 Page Website",
-            // "3 Page Website",
-            // "3 Page Website",
-          ]}
-        />
-
-        <PackageDeal
-          title={"Startup Web Package"}
-          originalPrice={"$596"}
-          price={"$399"}
-          description={
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, voluptates?"
-          }
-          details={["5 Stock Photos", "5 Page Website"]}
-        />
-
-        <PackageDeal
-          title={"Startup Web Package"}
-          originalPrice={"$1398"}
-          price={"$699"}
-          description={
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, voluptates?"
-          }
-          details={["5 Stock Photos", "5 Page Website"]}
-        />
+        {visiblePackageGroup?.packages.map((packageItem) => (
+          <PackageDeal key={packageItem.title} {...packageItem} />
+        ))}
       </div>
     </div>
   );
 };
 
 export default Packages;
+
+function PackageGroupSelector({ activeGroup, setActiveGroup, groups }) {
+  return (
+    <div className="flex flex-wrap justify-center">
+      {groups?.map((group) => (
+        <button
+          onClick={() => setActiveGroup(group)}
+          className={`m-1 p-2 rounded border-2 ${
+            group == activeGroup ? "bg-blue-500 text-white" : ""
+          }`}
+          key={group}
+        >
+          {group}
+        </button>
+      ))}
+    </div>
+  );
+}
