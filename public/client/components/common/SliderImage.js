0e44615eaffbaadcc86d5d30d3c0dca71ed6ec6d
ShinteiMai <stevenhanselgo@gmail.com>
2020-08-20 10:24:57 +0700

    Finished refactoring carousel

--- public/client/components/common/SliderImage.js
+++ public/client/components/common/SliderImage.js
@@ -1,13 +1,15 @@
 import React from "react";
 
-
 const SliderImage = (props) => {
   return (
-  <div className={props.className}>
-    <img src={props.image} alt="Image Not Found" className="slider-center"/>
-  </div>
-);
-}
-
+    <div className={props.className}>
+      <img
+        src={props.image}
+        alt="Image Not Found"
+        className={props.rounded ? "rounded w-full" : "slider-center"}
+      />
+    </div>
+  );
+};
 
 export default SliderImage;
