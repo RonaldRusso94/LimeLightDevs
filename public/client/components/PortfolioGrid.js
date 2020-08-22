81796b36ab0e25a57741e530db8497112893f08e
LEvinson2504 <ldsouza2504@gmail.com>
2020-08-22 13:23:48 +0530

    Now works with an array of image srcs, wraps to next row and has a fixed width so theres a bit of room for images to be little different sizes

--- public/client/components/PortfolioGrid.js
+++ public/client/components/PortfolioGrid.js
@@ -1,17 +1,37 @@
 import React from "react";
 
 const GridItem = ({ img }) => {
-    return <div>
-        <img src={img} />
+    return <div className="my-2 mx-2">
+        <img src={img} className="sm:w-64"/>
     </div>
 }
 
-const PortfolioGrid = () => {
-    return <section className="flex mx-20 bg-100">
-        <GridItem img="https://via.placeholder.com/300x450" />
-        <GridItem img="https://via.placeholder.com/300x450" />
-        <GridItem img="https://via.placeholder.com/300x450" />
-        <GridItem img="https://via.placeholder.com/300x450" />
+const imageStub = [
+    {
+        src: "https://via.placeholder.com/300x450"
+    },
+    {
+        src: "https://via.placeholder.com/400x450"
+    },
+    {
+        src: "https://via.placeholder.com/300x450"
+    },
+    {
+        src: "https://via.placeholder.com/500x450"
+    },
+    {
+        src: "https://via.placeholder.com/400x450"
+    },
+    {
+        src: "https://via.placeholder.com/300x450"
+    },
+]
+
+const PortfolioGrid = (props) => {
+    return <section className="flex flex-wrap content-start lg:mx-20 bg-100">
+        {imageStub.map( image => 
+            <GridItem img={image.src} />
+        )}
     </section>
 }
 
