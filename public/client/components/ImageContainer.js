6f768f81bbe3a156d5d338413c466d2e3419cff3
Ronald Russo <RonaldRusso94@yahoo.com>
2020-08-24 21:03:08 -0400

    Update ImageContainer.js

--- public/client/components/ImageContainer.js
+++ public/client/components/ImageContainer.js
@@ -4,16 +4,16 @@ const ImageContainer = ({
   children,
   className,
   content,
-  // title,
-  // description,
+  title,
+  description,
   img,
 }) => {
   return (
     <div className={`${className} flex`}>
       <div className="sm:w-1/2 flex flex-col items-center justify-center">
         {content}
-        {/* <h1 className="text-2xl">{title}</h1>
-        <p>{description}</p> */}
+        <h1 className="text-2xl">{title}</h1>
+        <p>{description}</p>
       </div>
       <div className="sm:w-1/2 flex flex-col items-center justify-center">
         <p>
