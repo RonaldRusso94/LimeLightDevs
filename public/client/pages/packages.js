8bd93d18556239711b717b907260b910e2aaa68c
kacper.sledz <casper.sledx@gmail.com>
2020-08-28 17:22:50 +0200

    #23 add contact form primary

--- public/client/pages/packages.js
+++ public/client/pages/packages.js
@@ -1,11 +1,13 @@
 import Layout from "../components/layout/Layout";
 import Packages from "../components/Packages";
+import ContactFormPrimary from "../components/ContactFormPrimary";
 
 const packages = () => {
   return (
     <Layout>
       <div className="md:mx-8 mx-1 bg-green-400 p-4">
         <Packages />
+        <ContactFormPrimary />
       </div>
     </Layout>
   );
