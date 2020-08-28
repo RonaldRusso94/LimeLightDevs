30139d5a410b0252363b668458b7e3fd695377f9
Tyler_Zoltowski <zoltowskit@gmail.com>
2020-08-28 17:28:22 -0400

    Changes to the Layout Component to accept Css Classes and setting default classes

--- public/client/components/layout/Layout.js
+++ public/client/components/layout/Layout.js
@@ -1,14 +1,19 @@
 import TopNavbar from "./TopNavbar";
 import Navbar from "./Navbar";
 
-const Layout = ({ children }) => {
+const Layout = ({ children, className}) => {
+  const containerClasses = className ||  "mx-1 md:mx-8 bg-green-400 px-8 py-4 md:px-12 md:py-8" //"md:mx-8 mx-1 bg-green-400 p-4"
   return (
     <>
       <nav>
         <TopNavbar />
         <Navbar />
       </nav>
-      <main className="mt-2">{children}</main>
+      <main className="mt-2">
+        <div className={containerClasses}>
+          {children}
+        </div>
+      </main>
     </>
   );
 };
