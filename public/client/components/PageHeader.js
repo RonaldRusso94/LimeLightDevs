0b8235e5c2d685ee0308c4fcf9b0419632d06709
LEvinson2504 <ldsouza2504@gmail.com>
2020-09-08 20:32:20 +0530

    passing style props

--- public/client/components/PageHeader.js
+++ public/client/components/PageHeader.js
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from "react";
 
-const PageHeader = ({title, subtitle}) => {
-    return (
-        <div className="container sm:m-3 sm:p-4">
-            <h1 className="text-6xl">{title}</h1>
-            <p className="text-xl">{subtitle}</p>
-        </div>
-    )
-}
+const PageHeader = ({ title, subtitle, className }) => {
+  return (
+    <div className={`${className} || container sm:m-3 sm:p-4`}>
+      <h1 className="text-6xl">{title}</h1>
+      <p className="text-xl">{subtitle}</p>
+    </div>
+  );
+};
 
-export default PageHeader
+export default PageHeader;
