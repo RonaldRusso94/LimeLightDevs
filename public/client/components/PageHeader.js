315fa58678bf0588c735ec68857dadff8436bfbc
Tyler_Zoltowski <zoltowskit@gmail.com>
2020-09-08 21:31:02 -0400

    Fixing boolean error

--- public/client/components/PageHeader.js
+++ public/client/components/PageHeader.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const PageHeader = ({ title, subtitle, className }) => {
   return (
-    <div className={`${className} || container sm:m-3 sm:p-4`}>
+    <div className={className || "container sm:m-3 sm:p-4"}>
       <h1 className="text-6xl">{title}</h1>
       <p className="text-xl">{subtitle}</p>
     </div>
